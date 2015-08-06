5f1183d82a1ff284b2caa6b290be5a0f9e58682f
atomicleopard <nick@atomicleopard.com>
2015-08-06 19:50:29 +1000

    Nick O: Further hacking on a jpa2/hibernate module which is xml free

--- src/main/java/com/atomicleopard/thundr/jpa/hibernate/HibernateModule.java
+++ src/main/java/com/atomicleopard/thundr/jpa/hibernate/HibernateModule.java
@@ -1,18 +1,84 @@
 package com.atomicleopard.thundr.jpa.hibernate;
 
+import java.util.Properties;
+import java.util.WeakHashMap;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.jpa.internal.EntityManagerFactoryImpl;
+import org.hibernate.service.ServiceRegistry;
+
 import com.threewks.thundr.injection.BaseModule;
+import com.threewks.thundr.injection.InjectionContext;
 import com.threewks.thundr.injection.UpdatableInjectionContext;
 
+/**
+ * Configures a basic, persistence.xml free Hibernate SessionFactory (for pure hibernate use) and
+ * EntityManagerFactory (for JPA2 use).
+ *
+ * Configuration is obtained from a {@link PersistenceUnit} configuration object which must be configured before applications start.
+ */
 public class HibernateModule extends BaseModule {
+	private WeakHashMap<String, SessionFactory> sessionFactories = new WeakHashMap<>();
+	private WeakHashMap<String, EntityManagerFactory> entityManagerFactories = new WeakHashMap<>();
+
 	@Override
 	public void start(UpdatableInjectionContext injectionContext) {
 		super.start(injectionContext);
-		
-		//
-		// persistence-units%dev=users-dev-db.properties,accounts-dev-db.properties
-		// persistence-units%prod=users-prod-db.properties,accounts-prod-db.properties
-		// persistence-dev.xml
-		// hibernate-dialect=HSQLDB
-		
+		PersistenceUnit unit = injectionContext.get(PersistenceUnit.class);
+		createAndInjectSessionFactoryAndEntityManagerFactoryForPersistenceUnit(injectionContext, unit);
 	}
+
+	protected void createAndInjectSessionFactoryAndEntityManagerFactoryForPersistenceUnit(UpdatableInjectionContext injectionContext, PersistenceUnit unit) {
+		Configuration configuration = new Configuration();
+		for (Class<?> entityClass : unit.getEntityClasses()) {
+			configuration.addAnnotatedClass(entityClass);
+		}
+		for (Class<? extends AttributeConverter<?, ?>> convertor : unit.getAttributeConvertors()) {
+			configuration.addAttributeConverter(convertor);
+		}
+		Properties cfgProperties = configuration.getProperties();
+		cfgProperties.put(Environment.DATASOURCE, unit.getDataSource());
+		cfgProperties.put(Environment.DIALECT, unit.getDialect());
+
+		StandardServiceRegistry serviceRegistry = createServiceRegistry(configuration);
+		SessionFactory sessionFactory = createSessionFactory(configuration, serviceRegistry);
+
+		// LocalSessionFactoryBuilder from Spring ORM is a world of interesting low level hacks
+		// If you need to further customise hibernate, check it out and hack away
+		// http://grepcode.com/file_/repo1.maven.org/maven2/org.springframework/spring-orm/4.1.6.RELEASE/org/springframework/orm/hibernate4/LocalSessionFactoryBuilder.java/?v=source
+
+		EntityManagerFactory entityManagerFactory = new EntityManagerFactoryImpl(unit.getTransactionType(), unit.isReleaseResourcesOnCloseEnabled(), unit.getSessionInterceptorClass(), configuration,
+				serviceRegistry, unit.getName());
+		sessionFactories.put(unit.getName(), sessionFactory);
+		entityManagerFactories.put(unit.getName(), entityManagerFactory);
+		injectionContext.inject(sessionFactory).named(unit.getName()).as(SessionFactory.class);
+		injectionContext.inject(entityManagerFactory).named(unit.getName()).as(EntityManagerFactory.class);
+	}
+
+	@Override
+	public void stop(InjectionContext injectionContext) {
+		super.stop(injectionContext);
+		for (EntityManagerFactory factory : entityManagerFactories.values()) {
+			factory.close();
+		}
+		for (SessionFactory sessionFactory : sessionFactories.values()) {
+			sessionFactory.close();
+		}
+	}
+
+	protected StandardServiceRegistry createServiceRegistry(Configuration configuration) {
+		return new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+	}
+
+	protected SessionFactory createSessionFactory(Configuration configuration, ServiceRegistry serviceRegistry) {
+		return configuration.buildSessionFactory(serviceRegistry);
+	}
+
 }
--- src/main/java/com/atomicleopard/thundr/jpa/hibernate/PersistenceUnit.java
+++ src/main/java/com/atomicleopard/thundr/jpa/hibernate/PersistenceUnit.java
@@ -1,33 +1,121 @@
 package com.atomicleopard.thundr.jpa.hibernate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.persistence.AttributeConverter;
+import javax.persistence.spi.PersistenceUnitTransactionType;
 import javax.sql.DataSource;
 
+import org.hibernate.Interceptor;
 import org.hibernate.dialect.Dialect;
 
 public class PersistenceUnit {
-	protected String name;
+	protected String name = "default";
 	protected DataSource dataSource;
 	protected Map<String, String> properties = new HashMap<>();
-	
-	public PersistenceUnit(String name, Dialect dialect, DataSource dataSource, Map<String, String> properties) {
-		super();
-		this.name = name;
-		this.dialect = dialect;
-		this.dataSource = dataSource;
-		this.properties = properties;
-	}
-	
-	public PersistenceUnit(String name, Map<String, String> properties) {
-		super();
-		this.name = name;
-		this.dialect = dialect;
-		this.dataSource = dataSource;
-		this.properties = properties;
-	}
-	
+	protected List<Class<?>> entities = new ArrayList<>();
+	protected Dialect dialect;
+	protected PersistenceUnitTransactionType transactionType = PersistenceUnitTransactionType.RESOURCE_LOCAL;
+	protected boolean releaseResourcesOnCloseEnabled = false;
+	protected Class<? extends Interceptor> sessionInterceptorClass;
+	protected List<Class<? extends AttributeConverter<?, ?>>> convertors = new ArrayList<>();
 
-	
+	// TODO - Dialect should be totally derivable from all known datasources
+	public PersistenceUnit(DataSource dataSource, Dialect dialect) {
+		super();
+		this.dataSource = dataSource;
+		this.dialect = dialect;
+	}
+
+	public PersistenceUnit withName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public PersistenceUnit withProperty(String key, String value) {
+		this.properties.put(key, value);
+		return this;
+	}
+
+	public PersistenceUnit withProperties(Map<String, String> properties) {
+		this.properties.putAll(properties);
+		return this;
+	}
+
+	public PersistenceUnit withEntity(Class<?> entity) {
+		this.entities.add(entity);
+		return this;
+	}
+
+	public PersistenceUnit withEntities(Class<?>... entities) {
+		this.entities.addAll(Arrays.asList(entities));
+		return this;
+	}
+
+	public PersistenceUnit withTransactionType(PersistenceUnitTransactionType transactionType) {
+		this.transactionType = transactionType;
+		return this;
+	}
+
+	public PersistenceUnit withConvertor(Class<? extends AttributeConverter<?, ?>> convertor) {
+		this.convertors.add(convertor);
+		return this;
+	}
+
+	@SuppressWarnings("unchecked")
+	public PersistenceUnit withConvertors(Class<? extends AttributeConverter<?, ?>>... convertors) {
+		this.convertors.addAll(Arrays.asList(convertors));
+		return this;
+	}
+
+	public PersistenceUnit withReleaseResourcesOnCloseEnabled(boolean releaseResourcesOnCloseEnabled) {
+		this.releaseResourcesOnCloseEnabled = releaseResourcesOnCloseEnabled;
+		return this;
+	}
+
+	public PersistenceUnit withSessionInterceptorClass(Class<? extends Interceptor> sessionInterceptorClass) {
+		this.sessionInterceptorClass = sessionInterceptorClass;
+		return this;
+	}
+
+	public Dialect getDialect() {
+		return dialect;
+	}
+
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	public List<Class<?>> getEntityClasses() {
+		return entities;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Map<String, String> getProperties() {
+		return properties;
+	}
+
+	public PersistenceUnitTransactionType getTransactionType() {
+		return transactionType;
+	}
+
+	public Class<? extends Interceptor> getSessionInterceptorClass() {
+		return sessionInterceptorClass;
+	}
+
+	public boolean isReleaseResourcesOnCloseEnabled() {
+		return releaseResourcesOnCloseEnabled;
+	}
+
+	public List<Class<? extends AttributeConverter<?, ?>>> getAttributeConvertors() {
+		return convertors;
+	}
+
 }
